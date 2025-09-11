64c42e047ecf1c1b64df467f7ac9700dbfc99c03
Sujal366 <sujalgandhi786@gmail.com>
2025-09-11 15:18:16 +0530

    updated CORS

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,21 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://equipment-maintenance-tracker.sujalgandhi.me",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/equipment", require("./routes/equipment"));
 app.use("/api/workorders", require("./routes/workorders"));
 app.use("/api/reports", require("./routes/reports"));
 
 const PORT = process.env.PORT || 5001;
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/equipment_maintenance";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
